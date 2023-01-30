9f5d248f17982e19939f5c13385e7d56e63466bc
Daniel Stone <danielstjr@protonmail.com>
2023-01-29 19:07:17 -0600

    Add Experiences page, create base template, simplify nav

--- src/frontend.rs
+++ src/frontend.rs
@@ -1,6 +1,41 @@
-use rocket_dyn_templates::{Template, context};
+use rocket::serde::Serialize;
+use rocket_dyn_templates::Template;
+
+#[derive(Serialize)]
+struct NavItem {
+    text: &'static str,
+    link: &'static str,
+    active: bool,
+}
+
+#[derive(Serialize)]
+struct NavList {
+    nav_items: Vec<NavItem>
+}
 
 #[get("/")]
 pub fn home() -> Template {
-    Template::render("home", context! {})
+    Template::render("home", &build_nav_list(0))
+}
+
+#[get("/experiences")]
+pub fn experiences() -> Template {
+    Template::render("experiences", &build_nav_list(1))
+}
+
+fn build_nav_list(i: usize) -> NavList {
+    NavList {
+        nav_items: vec![
+            NavItem {
+                text: "Home",
+                link: "../",
+                active: i == 0,
+            },
+            NavItem {
+                text: "Experiences & Education",
+                link: "../experiences",
+                active: i == 1,
+            },
+        ]
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #[macro_use] extern crate rocket;
 
+use rocket::{Build, Rocket};
 use rocket_dyn_templates::Template;
 
 mod frontend;
 
 #[launch]
-fn rocket() -> _ {
+fn rocket() -> Rocket<Build> {
     rocket::build()
-        .mount("/", routes![frontend::home])
+        .mount("/", routes![frontend::home, frontend::experiences])
         .attach(Template::fairing())
 }
