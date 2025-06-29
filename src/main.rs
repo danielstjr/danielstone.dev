52fd3cb39bbfbf05481d1cb1d94e01c9584a22c7
Daniel Stone <danielstjr@protonmail.com>
2025-06-29 14:17:59 -0500

    Call to action and formatting cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use] extern crate rocket;
 
 use rocket::{Build, Rocket};
+use rocket::fs::{relative, FileServer};
 use rocket_dyn_templates::Template;
 
 mod frontend;
@@ -8,6 +9,7 @@ mod frontend;
 #[launch]
 fn rocket() -> Rocket<Build> {
     rocket::build()
+        .mount("/assets", FileServer::from(relative!("assets")))
         .mount("/", routes![frontend::home, frontend::experiences])
         .attach(Template::fairing())
 }
